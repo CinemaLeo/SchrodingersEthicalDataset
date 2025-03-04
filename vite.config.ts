f13e6bd18924c4972966269d4b1d3ee2c37923a7
CinemaLeo <lbgb@hotmail.co.uk>
2025-03-04 16:45:29 +0800

    Add GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: ["proud-donkeys-tease.loca.lt"]
-  }
+  base: '/SchrodingersEthicalDataset/',
+  server: {  }
 })
